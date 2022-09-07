26e0fde0e78d81b8457df40948f555832cb38ef1
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-09-07 15:46:08 -0400

    Completed #1 View Source

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
-
 /* javascript */
+//alert("Hello...");
+console.log("hello this is a console message");
+
+let box = document.querySelector(".find-width")
+let width = box.offsetWidth;
+let height = box.offsetHeight;
+console.log({ width, height });
+
+// console.log(document.querySelector(".find-width").offsetWidth);
+// console.log(document.querySelector(".find-width").offsetHeight);
